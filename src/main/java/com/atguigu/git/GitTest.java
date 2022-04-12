053d0708e84489f3c3981ba0244093597f06832c
jimi123456jm <103477345+jimi123456jm@users.noreply.github.com>
2022-04-13 00:49:39 +0800

    pullTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hotfix Test");
         System.out.println("push test");
         System.out.println("push ssh test");
+        System.out.println("pull test");
     }
 }
